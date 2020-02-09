1470a77c0e07c282c48e4a64b3e6baf1aa9b33fb
Victor Nogueira <felladrin@gmail.com>
2020-02-09 17:11:18 +0000

    Don't start a new agent if there's one already running

--- .devcontainer/activate-ssh-agent-on-linux-or-mac.sh
+++ .devcontainer/activate-ssh-agent-on-linux-or-mac.sh
@@ -1,2 +1,8 @@
-eval "$(ssh-agent -s)"
+RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
+
+if [ "$RUNNING_AGENT" = "0" ]
+then
+  eval "$(ssh-agent -s)"
+fi
+
 ssh-add $HOME/.ssh/id_rsa
