e73197d8f8a166d87bc9003d47cea96807366b5c
Victor Nogueira <felladrin@gmail.com>
2020-02-09 19:36:19 +0000

    Make `addListener` and `removeListener` private
    As ideally they shouldn't be invoked from the dispatcher.

--- ts/index.ts
+++ ts/index.ts
@@ -1,7 +1,7 @@
 type TypedEventListener<T> = (data?: T) => void;
 
 export type TypedEvent<T = void> = {
-  addListener(listener: TypedEventListener<T>, listenOnlyOnce?:boolean): void;
+  addListener(listener: TypedEventListener<T>, listenOnlyOnce?: boolean): void;
   removeListener(listener: TypedEventListener<T>): void;
 }
 
@@ -9,8 +9,13 @@ export class TypedEventDispatcher<T = void> {
   private readonly listeners: Array<TypedEventListener<T>> = [];
   private readonly oneTimeListeners: Array<TypedEventListener<T>> = [];
 
+  public dispatch(data?: T): void {
+    this.callListeners(data);
+    this.wipeOneTimeListeners();
+  }
+
   public get getter(): TypedEvent<T> {
-    const typedEvent:TypedEvent<T> = {
+    const typedEvent: TypedEvent<T> = {
       addListener: this.addListener,
       removeListener: this.removeListener
     };
@@ -21,23 +26,18 @@ export class TypedEventDispatcher<T = void> {
     return typedEvent;
   }
 
-  public addListener(listener: TypedEventListener<T>, listenOnlyOnce = false): void {
+  private addListener(listener: TypedEventListener<T>, listenOnlyOnce = false): void {
     this.listeners.push(listener);
     if (listenOnlyOnce) {
       this.oneTimeListeners.push(listener);
     }
   }
 
-  public removeListener(listener: TypedEventListener<T>): void {
+  private removeListener(listener: TypedEventListener<T>): void {
     const indexOfListener = this.listeners.indexOf(listener);
     if (indexOfListener >= 0) {
       this.listeners.splice(indexOfListener, 1);
     }
-  }
-
-  public dispatch(data?: T): void {
-    this.callListeners(data);
-    this.wipeOneTimeListeners();
   }
 
   private callListeners(data?: T): void {
