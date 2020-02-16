4fbc54550f6901e78d0ea15250bc9a679bf3c2c4
Victor Nogueira <felladrin@gmail.com>
2020-02-16 02:56:20 +0000

    Update Dev Container

--- .devcontainer/activate-ssh-agent-on-linux-or-mac.sh
+++
@@ -1,8 +0,0 @@
-RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
-
-if [ "$RUNNING_AGENT" = "0" ]
-then
-  eval "$(ssh-agent -s)"
-fi
-
-ssh-add $HOME/.ssh/id_rsa
