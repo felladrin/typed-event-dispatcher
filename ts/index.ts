d2daa5a85eb0e6d3990fd7b456e787b896db71fa
Victor Nogueira <felladrin@gmail.com>
2020-03-21 04:53:31 +0000

    Simplify the code by converting the functions to a class

--- ts/index.ts
+++ ts/index.ts
@@ -1,27 +1,31 @@
 export type TypedEventListener<T> = T extends void ? () => void : (data: T) => void;
 
-export type TypedEvent<T> = {
-  addListener: (listener: TypedEventListener<T>, listenOnlyOnce?: boolean) => void;
-  removeListener: (listener: TypedEventListener<T>) => void;
-};
+export type TypedEvent<T> = Event<T>;
 
 type TypedEventDispatcherExtended<T> = TypedEventDispatcher<T> & {
   listeners: TypedEventListener<T>[];
   oneTimeListeners: TypedEventListener<T>[];
 };
 
-function addListener<T>(listener: TypedEventListener<T>, listenOnlyOnce?: boolean): void;
-function addListener<T>(this: TypedEventDispatcherExtended<T>, listener: TypedEventListener<T>, listenOnlyOnce = false): void {
-  const { listeners, oneTimeListeners } = this;
-  listeners.push(listener);
-  if (listenOnlyOnce) oneTimeListeners.push(listener);
-}
+class Event<T> {
+  constructor(dispatcher: TypedEventDispatcher<T>) {
+    this.addListener = this.addListener.bind(dispatcher);
+    this.removeListener = this.removeListener.bind(dispatcher);
+  }
 
-function removeListener<T>(listener: TypedEventListener<T>): void;
-function removeListener<T>(this: TypedEventDispatcherExtended<T>, listener: TypedEventListener<T>): void {
-  const { listeners } = this;
-  const indexOfListener = listeners.indexOf(listener);
-  if (indexOfListener >= 0) listeners.splice(indexOfListener, 1);
+  public addListener(listener: TypedEventListener<T>, listenOnlyOnce?: boolean): void;
+  public addListener(this: TypedEventDispatcherExtended<T>, listener: TypedEventListener<T>, listenOnlyOnce = false): void {
+    const { listeners, oneTimeListeners } = this;
+    listeners.push(listener);
+    if (listenOnlyOnce) oneTimeListeners.push(listener);
+  }
+
+  public removeListener(listener: TypedEventListener<T>): void;
+  public removeListener(this: TypedEventDispatcherExtended<T>, listener: TypedEventListener<T>): void {
+    const { listeners } = this;
+    const indexOfListener = listeners.indexOf(listener);
+    if (indexOfListener >= 0) listeners.splice(indexOfListener, 1);
+  }
 }
 
 export class TypedEventDispatcher<T = void> {
@@ -32,10 +36,7 @@ export class TypedEventDispatcher<T = void> {
     });
   }
 
-  public readonly getter: TypedEvent<T> = {
-    addListener: addListener.bind(this),
-    removeListener: removeListener.bind(this)
-  };
+  public readonly getter: TypedEvent<T> = new Event<T>(this);
 
   public dispatch(data: T): void;
   public dispatch(this: TypedEventDispatcherExtended<T>, data: T): void {
