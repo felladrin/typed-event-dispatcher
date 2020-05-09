363352c3cced2db52fd061c2448a53782c78e9d5
Victor Nogueira <felladrin@gmail.com>
2020-05-09 14:49:45 -0400

    Avoid confusion with 'this'  keyword by using functions where possible

--- typed-event-dispatcher.ts
+++ typed-event-dispatcher.ts
@@ -14,34 +14,33 @@ export type TypedEvent<T = void> = {
   removeListener(listener: TypedEventListener<T>): void;
 };
 
-type TypedEventDispatcherExtended<T> = TypedEventDispatcher<T> & {
-  database: { listener: TypedEventListener<T>; listenOnlyOnce: boolean }[];
-};
+type TypedEventDispatcherDatabase<T> = {
+  listener: TypedEventListener<T>;
+  listenOnlyOnce?: boolean;
+}[];
 
-class TypedEventGetter<T> implements TypedEvent<T> {
-  constructor(dispatcher: TypedEventDispatcher<T>) {
-    const { addListener, removeListener } = this as TypedEvent<T>;
-    this.addListener = addListener.bind(dispatcher);
-    this.removeListener = removeListener.bind(dispatcher);
-  }
+function addListener<T>(
+  database: TypedEventDispatcherDatabase<T>,
+  listener: TypedEventListener<T>,
+  listenOnlyOnce?: boolean
+): void {
+  if (!database.some((record) => record.listener == listener))
+    database.unshift({ listener: listener, listenOnlyOnce: listenOnlyOnce });
+}
 
-  public addListener(
-    this: TypedEventDispatcherExtended<T>,
-    listener: TypedEventListener<T>,
-    listenOnlyOnce = false
-  ): void {
-    const { database } = this;
-    if (!database.some((record) => record.listener == listener))
-      database.unshift({ listener, listenOnlyOnce });
-  }
+function removeListener<T>(
+  database: TypedEventDispatcherDatabase<T>,
+  listener: TypedEventListener<T>
+): void {
+  const index = database.findIndex((record) => record.listener == listener);
+  if (index >= 0) database.splice(index, 1);
+}
 
-  public removeListener(
-    this: TypedEventDispatcherExtended<T>,
-    listener: TypedEventListener<T>
-  ): void {
-    const { database } = this;
-    const index = database.findIndex((record) => record.listener == listener);
-    if (index >= 0) database.splice(index, 1);
+function dispatch<T>(database: TypedEventDispatcherDatabase<T>, data: T): void {
+  for (let index = database.length - 1; index >= 0; index--) {
+    const { listener, listenOnlyOnce } = database[index];
+    listener(data);
+    if (listenOnlyOnce) database.splice(index, 1);
   }
 }
 
@@ -49,21 +48,21 @@ export class TypedEventDispatcher<T = void> {
   constructor();
   constructor(dispatchedDataSample: T);
   constructor() {
-    Object.defineProperties(this, { database: { value: [] } });
-    this.getter = new TypedEventGetter<T>(this);
+    const database: TypedEventDispatcherDatabase<T> = [];
+    this.getter = {
+      addListener: (
+        listener: TypedEventListener<T>,
+        listenOnlyOnce = false
+      ): void => addListener(database, listener, listenOnlyOnce),
+      removeListener: (listener: TypedEventListener<T>): void =>
+        removeListener(database, listener),
+    };
+    this.dispatch = (data: T): void => dispatch(database, data);
   }
 
   /** Holds the respective TypedEvent of this dispatcher. */
   public readonly getter: TypedEvent<T>;
 
   /** Dispatches the TypedEvent, optionally passing some data. */
-  public dispatch(data: T): void;
-  public dispatch(this: TypedEventDispatcherExtended<T>, data: T): void {
-    const { database } = this;
-    for (let index = database.length - 1; index >= 0; index--) {
-      const { listener, listenOnlyOnce } = database[index];
-      listener(data);
-      if (listenOnlyOnce) database.splice(index, 1);
-    }
-  }
+  public dispatch: (data: T) => void;
 }
