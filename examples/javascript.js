1e192b9858df74a5cfd77fb77a3dcacb80bde8c2
Victor Nogueira <felladrin@gmail.com>
2023-04-20 18:09:54 +0300

    Add examples using GitHub Blocks

---
+++ examples/javascript.js
@@ -0,0 +1,55 @@
+const { TypedEventDispatcher } = require("typed-event-dispatcher");
+
+class Counter {
+  constructor() {
+    this.count = 0;
+
+    //--------------------------------------------//
+    // STEP 1: Create a private event dispatcher. //
+    //--------------------------------------------//
+    this.onCountIncreasedDispatcher = new TypedEventDispatcher(this.count);
+  }
+
+  //---------------------------------------//
+  // STEP 2: Create a public event getter. //
+  //---------------------------------------//
+  get onCountIncreased() {
+    return this.onCountIncreasedDispatcher.getter;
+  }
+
+  increaseCountOncePerSecond() {
+    setInterval(() => {
+      this.increaseCount();
+
+      //----------------------------------------------------------//
+      // STEP 3: Dispatch the event so listeners can react to it. //
+      //----------------------------------------------------------//
+      this.onCountIncreasedDispatcher.dispatch(this.count);
+    }, 1000);
+  }
+
+  increaseCount() {
+    this.count++;
+  }
+}
+
+class Example {
+  constructor() {
+    this.counter = new Counter();
+  }
+
+  start() {
+    console.log("Starting count...");
+
+    //-------------------------------------------------------//
+    // STEP 4: Listen to events dispatched by other classes. //
+    //-------------------------------------------------------//
+    this.counter.onCountIncreased.addListener((count) => {
+      console.log(`Count increased to ${count}.`);
+    });
+
+    this.counter.increaseCountOncePerSecond();
+  }
+}
+
+new Example().start();
---
+++ examples/readme.md
@@ -0,0 +1,25 @@
+# Examples
+
+## JavaScript
+
+<BlockComponent
+  block={{
+    owner: "felladrin",
+    repo: "typed-event-dispatcher",
+    id: "sandbox",
+    type: "file",
+  }}
+  context={{"path":"examples/javascript.js"}}
+/>
+
+## TypeScript
+
+<BlockComponent
+  block={{
+    owner: "felladrin",
+    repo: "typed-event-dispatcher",
+    id: "sandbox",
+    type: "file",
+  }}
+  context={{"path":"examples/typescript.ts"}}
+/>
