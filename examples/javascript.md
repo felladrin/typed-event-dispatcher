591f348d8c2bbf3bc4e9ad4354fd3ce81442d174
Victor Nogueira <felladrin@gmail.com>
2023-04-20 18:14:59 +0300

    Add examples using GitHub Blocks (#243)

---
+++ examples/javascript.md
@@ -0,0 +1,13 @@
+# JavaScript Example
+
+<BlockComponent
+block={{
+    owner: "githubnext",
+    repo: "blocks-examples",
+    id: "sandbox",
+    type: "file",
+  }}
+context={{
+    path: "examples/javascript.js"
+  }}
+/>
---
+++ examples/typescript.ts
@@ -0,0 +1,51 @@
+import { TypedEventDispatcher } from "typed-event-dispatcher";
+
+class Counter {
+  //--------------------------------------------//
+  // STEP 1: Create a private event dispatcher. //
+  //--------------------------------------------//
+  private onCountIncreasedDispatcher = new TypedEventDispatcher<number>();
+
+  //---------------------------------------//
+  // STEP 2: Create a public event getter. //
+  //---------------------------------------//
+  public get onCountIncreased() {
+    return this.onCountIncreasedDispatcher.getter;
+  }
+
+  public increaseCountOncePerSecond() {
+    setInterval(() => {
+      this.increaseCount();
+
+      //----------------------------------------------------------//
+      // STEP 3: Dispatch the event so listeners can react to it. //
+      //----------------------------------------------------------//
+      this.onCountIncreasedDispatcher.dispatch(this.count);
+    }, 1000);
+  }
+
+  private count = 0;
+
+  private increaseCount() {
+    this.count++;
+  }
+}
+
+class Example {
+  private counter = new Counter();
+
+  public start() {
+    console.log("Starting count...");
+
+    //-------------------------------------------------------//
+    // STEP 4: Listen to events dispatched by other classes. //
+    //-------------------------------------------------------//
+    this.counter.onCountIncreased.addListener((count) => {
+      console.log(`Count increased to ${count}.`);
+    });
+
+    this.counter.increaseCountOncePerSecond();
+  }
+}
+
+new Example().start();
